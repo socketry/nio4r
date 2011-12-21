e70247a5d01d23a5ae82b6f05620cf6280d110e8
Tony Arcieri <tony.arcieri@gmail.com>
2011-12-21 15:17:09 -0800

    Initial NIO::Channel#blocking?
    NIO::Channels can operate in either blocking or non-blocking mode, just
    like BSD sockets can. This is different from normal Ruby IO objects,
    which don't have a sense of what mode they're in and instead just have
    different methods for performing blocking or nonblocking operations.

--- lib/nio/jruby/channel.rb
+++ lib/nio/jruby/channel.rb
@@ -1,10 +1,17 @@
 module NIO
   # Channels provide the glue between IO objects and the NIO subsystem
   class Channel
+    attr_reader :java_channel
+    
     # Create a new NIO::Channel from a Ruby IO object
     def initialize(io)
       @io = io      
       @java_channel = io.to_channel
+    end
+    
+    # Is this channel blocking?
+    def blocking?
+      @java_channel.isBlocking
     end
   end
 end
--- spec/nio/channel_spec.rb
+++ spec/nio/channel_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe NIO::Channel do
   it "constructs channels from IO objects" do
     channel = NIO::Channel.new(STDIN)
-    channel.should be_a(NIO::Channel)
+    channel.should be_a NIO::Channel
+  end
+  
+  it "knows blocking modes for channels" do
+    pipe, _ = IO.pipe
+    channel = pipe.channel
+    channel.should be_blocking
   end
 end
