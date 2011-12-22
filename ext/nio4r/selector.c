d5807be20a5e0366dfbaf48df7f5dff59ea9152e
Tony Arcieri <tony.arcieri@gmail.com>
2011-12-22 00:33:05 -0800

    Stub out NIO::Selector#register

--- ext/nio4r/selector.c
+++ ext/nio4r/selector.c
@@ -14,6 +14,8 @@ static VALUE NIO_Selector_allocate(VALUE klass);
 static void NIO_Selector_mark(struct NIO_Selector *loop);
 static void NIO_Selector_free(struct NIO_Selector *loop);
 
+static VALUE NIO_Selector_register(VALUE self, VALUE selectable, VALUE interest);
+
 /* Default number of slots in the buffer for selected monitors */
 #define SELECTED_BUFFER_SIZE 32
 
@@ -23,18 +25,20 @@ void Init_NIO_Selector()
     mNIO = rb_define_module("NIO");
     cNIO_Selector = rb_define_class_under(mNIO, "Selector", rb_cObject);
     rb_define_alloc_func(cNIO_Selector, NIO_Selector_allocate);
+
+    rb_define_method(cNIO_Selector, "register", NIO_Selector_register, 2);
 }
 
 static VALUE NIO_Selector_allocate(VALUE klass)
 {
-  struct NIO_Selector *selector = (struct NIO_Selector *)xmalloc(sizeof(struct NIO_Selector));
+    struct NIO_Selector *selector = (struct NIO_Selector *)xmalloc(sizeof(struct NIO_Selector));
 
-  selector->ev_loop = 0;
-  selector->selecting = selector->total_selected = 0;
-  selector->buffer_size = SELECTED_BUFFER_SIZE;
-  selector->selected_buffer = (struct NIO_Selected *)xmalloc(sizeof(struct NIO_Selected) * SELECTED_BUFFER_SIZE);
+    selector->ev_loop = ev_loop_new(0);
+    selector->selecting = selector->total_selected = 0;
+    selector->buffer_size = SELECTED_BUFFER_SIZE;
+    selector->selected_buffer = (struct NIO_Selected *)xmalloc(sizeof(struct NIO_Selected) * SELECTED_BUFFER_SIZE);
 
-  return Data_Wrap_Struct(klass, NIO_Selector_mark, NIO_Selector_free, selector);
+    return Data_Wrap_Struct(klass, NIO_Selector_mark, NIO_Selector_free, selector);
 }
 
 static void NIO_Selector_mark(struct NIO_Selector *selector)
@@ -43,9 +47,13 @@ static void NIO_Selector_mark(struct NIO_Selector *selector)
 
 static void NIO_Selector_free(struct NIO_Selector *selector)
 {
-  if(selector->ev_loop)
-      ev_loop_destroy(selector->ev_loop);
+    ev_loop_destroy(selector->ev_loop);
 
-  xfree(selector->selected_buffer);
-  xfree(selector);
+    xfree(selector->selected_buffer);
+    xfree(selector);
+}
+
+static VALUE NIO_Selector_register(VALUE self, VALUE selectable, VALUE interest)
+{
+    return Qnil;
 }
--- lib/nio/channel.rb
+++ lib/nio/channel.rb
@@ -1,6 +1,8 @@
 module NIO
   # Channels provide the glue between IO objects and the NIO subsystem
   class Channel
+    attr_reader :io
+
     # Create a new NIO::Channel from a Ruby IO object
     def initialize(io)
       @io = io
