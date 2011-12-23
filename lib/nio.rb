e5e852643a9e3d85f171d66c4f096e76979b7f61
Tony Arcieri <tony.arcieri@gmail.com>
2011-12-22 17:41:47 -0800

    NIO::ENGINE (and vicariously NIO.engine) describes the implementation
    It presently uses 'select' for pure-Ruby with Kernel.select, 'java' for
    Java NIO, and 'libev' for the C extension based on libev

--- lib/nio.rb
+++ lib/nio.rb
@@ -1,19 +1,27 @@
 require 'nio/version'
 require 'nio/core_ext'
 
+# New I/O for Ruby
+module NIO
+  # NIO implementation, one of the following (as a string):
+  # * select: in pure Ruby using Kernel.select
+  # * libev: as a C extension using libev
+  # * java: using Java NIO
+  def self.engine; ENGINE end
+end
+
 if ENV["NIO4R_PURE"]
   require 'nio/pure'
+  NIO::ENGINE = 'select'
 else
   if defined?(JRUBY_VERSION)
     require 'nio/jruby'
+    NIO::ENGINE = 'java'
   else
     require 'nio/channel'
     require 'nio4r_ext'
+    NIO::ENGINE = 'libev'
   end
-end
-
-# New I/O for Ruby
-module NIO
 end
 
 # TIMTOWTDI!!!
