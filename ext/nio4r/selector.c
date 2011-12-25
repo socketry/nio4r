a8d2fbde5b9c03b4002ed6213c86fc9bc9fa82a6
Tony Arcieri <tony.arcieri@gmail.com>
2011-12-25 01:31:20 -0700

    Initial NIO::Selector#select which runs the event loop (although nothing's attached yet)

--- ext/nio4r/selector.c
+++ ext/nio4r/selector.c
@@ -21,6 +21,7 @@ static void NIO_Selector_free(struct NIO_Selector *loop);
 /* Methods */
 static VALUE NIO_Selector_initialize(VALUE self);
 static VALUE NIO_Selector_register(VALUE self, VALUE selectable, VALUE interest);
+static VALUE NIO_Selector_select(int argc, VALUE *argv, VALUE self);
 static VALUE NIO_Selector_close(VALUE self);
 static VALUE NIO_Selector_closed(VALUE self);
 
@@ -28,6 +29,8 @@ static VALUE NIO_Selector_closed(VALUE self);
 static VALUE NIO_Selector_synchronize(VALUE self, VALUE (*func)(VALUE arg), VALUE arg);
 static VALUE NIO_Selector_unlock(VALUE lock);
 static VALUE NIO_Selector_register_synchronized(VALUE array);
+static VALUE NIO_Selector_select_synchronized(VALUE array);
+static VALUE NIO_Selector_run_evloop(void *ptr);
 
 /* Default number of slots in the buffer for selected monitors */
 #define SELECTED_BUFFER_SIZE 32
@@ -43,6 +46,7 @@ void Init_NIO_Selector()
 
     rb_define_method(cNIO_Selector, "initialize", NIO_Selector_initialize, 0);
     rb_define_method(cNIO_Selector, "register", NIO_Selector_register, 2);
+    rb_define_method(cNIO_Selector, "select", NIO_Selector_select, -1);
     rb_define_method(cNIO_Selector, "close", NIO_Selector_close, 0);
     rb_define_method(cNIO_Selector, "closed?", NIO_Selector_closed, 0);
 }
@@ -104,13 +108,6 @@ static VALUE NIO_Selector_initialize(VALUE self)
     return Qnil;
 }
 
-/* Register an IO object with the selector for the given interests */
-static VALUE NIO_Selector_register(VALUE self, VALUE io, VALUE interests)
-{
-    VALUE array = rb_ary_new3(3, self, io, interests);
-    return NIO_Selector_synchronize(self, NIO_Selector_register_synchronized, array);
-}
-
 static VALUE NIO_Selector_synchronize(VALUE self, VALUE (*func)(VALUE arg), VALUE arg)
 {
     VALUE lock;
@@ -125,11 +122,20 @@ static VALUE NIO_Selector_unlock(VALUE lock)
     rb_funcall(lock, rb_intern("unlock"), 0, 0);
 }
 
+/* Register an IO object with the selector for the given interests */
+static VALUE NIO_Selector_register(VALUE self, VALUE io, VALUE interests)
+{
+    VALUE array = rb_ary_new3(3, self, io, interests);
+    return NIO_Selector_synchronize(self, NIO_Selector_register_synchronized, array);
+}
+
+/* Internal implementation of register after acquiring mutex */
 static VALUE NIO_Selector_register_synchronized(VALUE array)
 {
     VALUE self, io, interests, selectables, monitor;
     VALUE args[2];
 
+    /* FIXME: Meh, these should probably be varargs */
     self = rb_ary_entry(array, 0);
     io = rb_ary_entry(array, 1);
     interests = rb_ary_entry(array, 2);
@@ -148,6 +154,57 @@ static VALUE NIO_Selector_register_synchronized(VALUE array)
     rb_hash_aset(selectables, io, monitor);
 
     return monitor;
+}
+
+/* Select from all registered IO objects */
+static VALUE NIO_Selector_select(int argc, VALUE *argv, VALUE self)
+{
+    VALUE timeout, array;
+
+    rb_scan_args(argc, argv, "01", &timeout);
+    array = rb_ary_new3(2, self, timeout);
+
+    return NIO_Selector_synchronize(self, NIO_Selector_select_synchronized, array);
+}
+
+/* Internal implementation of select with the selector lock held */
+static VALUE NIO_Selector_select_synchronized(VALUE array)
+{
+    VALUE self, timeout, result;
+    struct NIO_Selector *selector;
+
+    /* FIXME: Meh, these should probably be varargs */
+    self = rb_ary_entry(array, 0);
+    timeout = rb_ary_entry(array, 1);
+
+    Data_Get_Struct(self, struct NIO_Selector, selector);
+
+#if defined(HAVE_RB_THREAD_BLOCKING_REGION)
+    /* Ruby 1.9 lets us release the GIL and make a blocking I/O call */
+    result = rb_thread_blocking_region(NIO_Selector_run_evloop, selector, RUBY_UBF_IO, 0);
+#else
+    /* FIXME:
+    This makes a blocking call in 1.8 which will hang the entire
+    interpreter and prevent threads from being scheduled. I honestly don't
+    care about 1.8 that much. Let me know if you do and I can investigate
+    doing similar green thread workarounds as EM and cool.io */
+
+    TRAP_BEG;
+    result = NIO_Selector_run_evloop(selector);
+    TRAP_END;
+#endif
+
+    return result;
+}
+
+/* Run the libev event loop */
+static VALUE NIO_Selector_run_evloop(void *ptr)
+{
+    struct NIO_Selector *selector = (struct NIO_Selector *)ptr;
+
+    ev_loop(selector->ev_loop, EVLOOP_ONESHOT);
+
+    return Qnil;
 }
 
 static VALUE NIO_Selector_close(VALUE self)
