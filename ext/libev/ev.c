b479e24498f09839248784def35e2dda9e43bc56
Denis Washington <denis.washington@futurice.com>
2016-09-09 14:26:09 +0200

    Add missing "ruby/thread.h" include to ev.c

--- ext/libev/ev.c
+++ ext/libev/ev.c
@@ -39,6 +39,7 @@
 
 /* ########## NIO4R PATCHERY HO! ########## */
 #include "ruby.h"
+#include "ruby/thread.h"
 /* ######################################## */
 
 /* this big block deduces configuration from config.h */
