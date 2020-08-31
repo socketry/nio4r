867ac1bbded1591f7269bd4276b6b446019d1765
Bo <bo@bowild.com>
2020-08-31 22:43:40 +0300

    I think this was it

--- ext/nio4r/monitor.c
+++ ext/nio4r/monitor.c
@@ -66,6 +66,7 @@ static VALUE NIO_Monitor_allocate(VALUE klass)
 
 static void NIO_Monitor_mark(struct NIO_Monitor *monitor)
 {
+    rb_gc_mark(monitor->self); /* so GC won't move our reference to self */
 }
 
 static void NIO_Monitor_free(struct NIO_Monitor *monitor)
