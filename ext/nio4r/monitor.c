ce47daba57ae9e1f1eafd0840ad9c9e95712e2d1
Bo <bo@bowild.com>
2020-09-15 18:11:02 +0300

    Add `if` statement, even if redundant
    This short PR explores the possibility of adding a slightly redundant `if` statement...
    
    The if statement is probably redundant since `rb_gc_mark` tests for special non-allocated "objects" such as `nil`, `false`, embedded numbers etc').
    
    However: (1) since a NULL dereferencing issue was reported; and (2) GC tests this condition only after pushing the stack (so it's faster if we test for it); we might as well add this slightly redundant `if` statement and see if it helps.

--- ext/nio4r/monitor.c
+++ ext/nio4r/monitor.c
@@ -66,7 +66,8 @@ static VALUE NIO_Monitor_allocate(VALUE klass)
 
 static void NIO_Monitor_mark(struct NIO_Monitor *monitor)
 {
-    return rb_gc_mark(monitor->self);
+    if(monitor && monitor->self)
+        rb_gc_mark(monitor->self);
 }
 
 static void NIO_Monitor_free(struct NIO_Monitor *monitor)
