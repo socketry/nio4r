41469f699f2b007f8f0f640c1f4f2195df9aab43
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-02-06 10:30:12 +1300

    Be more explicit with why `pure?` is being used.

--- lib/nio.rb
+++ lib/nio.rb
@@ -12,9 +12,28 @@ module NIO
   def self.engine
     ENGINE
   end
+
+  def self.pure?(env = ENV)
+    # The user has explicitly opted in to non-native implementation:
+    if env["NIO4R_PURE"] == "true"
+      return true
+    end
+
+    # Native Ruby on Windows is not supported:
+    if (Gem.win_platform? && !defined?(JRUBY_VERSION))
+      return true
+    end
+
+    # M1 native extension is crashing on M1 (arm64):
+    if RUBY_PLATFORM =~ /darwin/ && RUBY_PLATFORM =~ /arm64/
+      return true
+    end
+
+    return false
+  end
 end
 
-if ENV["NIO4R_PURE"] == "true" || (Gem.win_platform? && !defined?(JRUBY_VERSION)) || (RUBY_PLATFORM =~ /darwin/ && RUBY_PLATFORM =~ /arm64/)
+if NIO.pure?
   require "nio/monitor"
   require "nio/selector"
   require "nio/bytebuffer"
